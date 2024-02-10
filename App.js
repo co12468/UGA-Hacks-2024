06441aac9e91c69a432e452ebaad5232830abdc8
co12468 <co12468@uga.edu>
2024-02-10 15:25:27 -0500

    Testing flex

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View 
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 1, backgroundColor: 'darkorange'}} />
+      <Text style={{flex: 5, backgroundColor: 'lightblue'}}>Howdy</Text>
+      <View style={{flex: 1, backgroundColor: 'blue'}} />
+      <Text style={styles.bigBlue}>Open up App.js to start working on your app!</Text>
       <Text>Hello World!</Text>
-      <Text>This is our app.</Text>
+      <Text style={styles.red}>This is our app.</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +26,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //backgroundColor: 'green',
+    //padding: 20,
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
