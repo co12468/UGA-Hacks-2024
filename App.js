86a5779d2a5ddfa662fa6b7b361ea5476c46be94
co12468 <co12468@uga.edu>
2024-02-10 14:01:15 -0500

    App is up

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>This is our app.</Text>
       <StatusBar style="auto" />
     </View>
   );
