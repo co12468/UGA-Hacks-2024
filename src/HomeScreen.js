8c258994412fff10a57a2e968812d4a05b5c1031
co12468 <co12468@uga.edu>
2024-02-11 07:39:20 -0500

    Can click on search items

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,29 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList, View, Text, TextInput, StyleSheet} from 'react-native';
+import {FlatList, View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 import jsonData from './cityData.json';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   const [text, setText] = useState('');
   const [filteredData, setFilteredData] = useState([]);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // console.log('Loaded JSON data:', jsonData);
-
     if (Array.isArray(jsonData?.wordList)) {
       setData(jsonData.wordList);
     } else {
       console.error("JSON data is not in the expected format");
     }
   }, []);
-    /*
-  const namesContainer = document.getElementById("namesContainer");
-  data.forEach(item => {
-    const button = document.createElement("button");
-    button.textContent = item.key;
-    namesContainer.appendChild(button)
-  });
-    */
+
   const handleSearch = (searchText) => {
     const filteredItems = (data || []).filter((item) =>
       item.toLowerCase().includes(searchText.toLowerCase())
@@ -32,18 +23,26 @@ const HomeScreen = () => {
     setText(searchText);
   };
 
+  const handleItemPress = (item) => {
+    console.log(`Clicked ${item}`);
+  };
+
   return (
-    <View style={{flex: 1, paddingTop: 20, paddingLeft: 10, alignItems: 'left'}}>
+    <View style={{ flex: 1, paddingTop: 20, paddingLeft: 10, alignItems: 'left' }}>
       <TextInput
-        style={{height: 40, fontSize: 20}}
+        style={{ height: 40, fontSize: 20 }}
         placeholder="Type here to search"
         onChangeText={handleSearch}
         value={text}
       />
       {text.length > 0 && (
         <FlatList
-            data={filteredData}
-            renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          data={filteredData}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleItemPress(item)}>
+              <Text style={styles.item}>{item}</Text>
+            </TouchableOpacity>
+          )}
         />
       )}
     </View>
