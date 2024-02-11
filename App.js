cb71a8ef7b705268fdebd0fb6b1e51bc09a0692f
co12468 <co12468@uga.edu>
2024-02-10 19:15:05 -0500

    updates

--- App.js
+++ App.js
@@ -1,42 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+import HomeScreen from './src/HomeScreen';
+import ReportScreen from './src/ReportScreen';
+import SearchScreen from './src/SearchScreen';
+import ServiceScreen from './src/ServiceScreen';
+
+const Tab = createBottomTabNavigator();
+
+const CustomTabBarButton = ({children, onPress}) => (
+  <TouchableOpacity
+    style={{
+      top: 0,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      padding: 15,
+      backgroundColor: 'rgba(100, 100, 200, 0.8)',
+    }}
+    onPress={onPress}
+  >
+    {children}
+  </TouchableOpacity>
+);
 
 export default function App() {
   return (
-    <View 
-      style={[
-        styles.container,
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
-        },
-      ]}>
-      <View style={{flex: 1, backgroundColor: 'darkorange'}} />
-      <Text style={{flex: 5, backgroundColor: 'lightblue'}}>Howdy</Text>
-      <View style={{flex: 1, backgroundColor: 'blue'}} />
-      <Text style={styles.bigBlue}>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!</Text>
-      <Text style={styles.red}>This is our app.</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarLabel: route.name,
+            tabBarButton: (props) => (
+              <CustomTabBarButton {...props}>
+                <Text style={{fontSize: 18, color: 'black'}}>{route.name}</Text>
+              </CustomTabBarButton>
+            ),
+            tabBarStyle: {backgroundColor: '#c0c0f9'},
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Reports" component={ReportScreen} />
+          <Tab.Screen name="Services" component={ServiceScreen} />
+          <Tab.Screen name="City Search" component={SearchScreen} />
+        </Tab.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: 'green',
-    //padding: 20,
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
+    flexDirection: 'column',
   },
 });
---
+++ src/ServiceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ServiceScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 24, color: 'blue'}}>Service Screen</Text>
+    </View>
+  );
+};
+
+export default ServiceScreen;
